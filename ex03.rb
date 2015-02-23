0419b27ac424604bffffaa33399961ab8d531d15
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-02-23 12:02:20 +0000

    Completed ex03

---
+++ ex03.rb
@@ -0,0 +1,17 @@
+puts "I will now count my cows."
+
+puts "Cows #{30 + 41 / 7}"
+
+puts 41 / 7
+puts "Calves #{1.5 * 3.6}"
+puts "Calves #{2 * 3.6}"
+puts "Calves #{1.5 * 4}"
+
+puts "Is it greater? #{10 > 20}"
+
+x = 10 > 20
+puts "Is it greater? #{x}"
+
+puts "Calves #{2 ** 4}" #=> 16
+
+puts "Is it greater? #{25 >= 25}"

4c6e5d138820dd101723a2302f363b05e16527e4
Pavel Redics <pavel.redics@gmail.com>
2015-02-23 10:29:51 +0000

    comments in code

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# Here is hidden text, and it's won't appear in terminal when programm will be excuted
+
+puts "I could have code like this" # some awesome code
+
+# That won't run in terminal as well:
+# puts "Hello Mark"
+
+puts "This will run"

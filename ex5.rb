8a8705d3823b9b2b5401e905c2e3b88d42305a8d
Mark Pokorny <iridium193@gmail.com>
2015-02-23 11:26:40 +0000

    Exercise 5 updated to remove ‘my_’ from variable names

--- ex5.rb
+++ ex5.rb
@@ -1,17 +1,17 @@
-my_name = "Mark"
-my_age = 29
-my_height = 172 # cm
-my_weight = 70 # kg
-my_eyes = 'Blue'
-my_teeth = 'White-ish'
-my_hair = 'Dark Blond'
+name = "Mark"
+age = 29
+height = 172 # cm
+weight = 70 # kg
+eyes = 'Blue'
+teeth = 'White-ish'
+hair = 'Dark Blond'
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} centimetres tall."
-puts "He's #{my_weight} kilogrammes in weight."
+puts "Let's talk about #{name}."
+puts "He's #{height} centimetres tall."
+puts "He's #{weight} kilogrammes in weight."
 puts "Actually that's not too heavy!"
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee!" # so true!
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee!" # so true!
 
 # This line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."

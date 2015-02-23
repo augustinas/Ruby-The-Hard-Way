aed186d1d79d1e3588862249fb0f81d7c534a946
Mark Pokorny <iridium193@gmail.com>
2015-02-23 11:24:55 +0000

    Exercise 5 added

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = "Mark"
+my_age = 29
+my_height = 172 # cm
+my_weight = 70 # kg
+my_eyes = 'Blue'
+my_teeth = 'White-ish'
+my_hair = 'Dark Blond'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} centimetres tall."
+puts "He's #{my_weight} kilogrammes in weight."
+puts "Actually that's not too heavy!"
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee!" # so true!
+
+# This line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."

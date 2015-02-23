a7b49d89dfca09a7c6c8a5e7bfcf2a50c7ac0aea
Jade King <dev@jadeking.co.uk>
2015-02-23 10:58:40 +0000

    Updated ex1.rb to explain a ruby comment #

--- ex1.rb
+++ ex1.rb
@@ -1,8 +1,9 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts "Yay! Printing."
-puts "I'd must rather you 'not'."
-puts 'I "said" do not touch this.'
+# adds a comment, this does not get shown in the code
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Printing."
+#puts "I'd must rather you 'not'."
+#puts 'I "said" do not touch this.'
 puts "Extra line"

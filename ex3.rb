a38fdbf93dfaf6a2bfd65b3359366c16306638e8
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-02-23 16:04:03 +0200

    Completed ex3.rb

---
+++ ex3.rb
@@ -0,0 +1,34 @@
+# Prints the intention to count chickens
+puts "I will now count my chickens:"
+
+#Prints the number of hens through string interpolation 
+puts "Hens #{25 + 30 / 6}"
+#Prints the number of roosters through string interpolation
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#Prints the intention to count eggs
+puts "Now I will count the eggs: "
+
+#Puts the value of a formula that counts the eggs
+puts 3 + 2 + 1 -5 + 4 % 2 - 1 / 4 + 6
+
+#Prints the question
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#Prints the Boolean value of the comparison below
+puts 3 + 2 < 5 -7
+
+#The next 2 lines answer simple questions through string interpolation
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+#A sudden realisation
+puts "Oh, that's why it's false."
+
+#Announcing some further research
+puts "How about some more."
+
+#The next 3 lines answer some tricky questions with the help of comparison operators and string interpolation
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"

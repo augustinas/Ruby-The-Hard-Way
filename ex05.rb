02a643d667784dc6fdbe1eeb2844fce0aa3de11b
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-02-23 12:35:00 +0000

    Completed ex 05

---
+++ ex05.rb
@@ -0,0 +1,7 @@
+group_name = 'Jade & Gus'
+group_age = 27 + 28
+pair_number = 3
+
+puts "In our group is #{group_name}"
+puts "Our group's combined age is #{group_age}"
+puts "Group age multiplied by pair number #{group_age * pair_number}"

f090ef1e9fbfac4f68bc789241643a6937f538e7
Pavel Redics <pavel.redics@gmail.com>
2015-02-23 11:39:50 +0000

    writing funny joke

---
+++ ex6.rb
@@ -0,0 +1,25 @@
+# assigning variable with integer 10
+types_of_people = 10
+
+# performing string interpolation
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}."
+
+# printing out x variable
+puts x
+puts y
+
+puts 'I said: #{x}'
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that jose so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
